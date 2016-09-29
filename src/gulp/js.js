6c8692f94bc33c53649a1e04384a269d546c825b
lyudakucher <kucherlyuda@gmail.com>
2016-09-29 21:59:26 +0300

    finish

--- src/gulp/js.js
+++ src/gulp/js.js
@@ -5,6 +5,11 @@ gulp.task('copy', function() {
         .src('src/js/**/*.js')
         .pipe(gulp.dest('build/js'));
 });
+gulp.task('copy', function() {
+    return gulp
+        .src('node_modules/bootstrap/dist/css/bootstrap.min.css')
+        .pipe(gulp.dest('build/css'));
+});
 
 gulp.task('copy:watch', function () {
     gulp.watch('src/js/**/*.js', ['copy'])
